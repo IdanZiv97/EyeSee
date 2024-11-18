3677ecb4d1959b618d7da478802e8d7d17e5c704
IdanZiv97 <idanziv7@gmail.com>
2024-11-18 11:45:43 +0200

    initial specific date query

--- Server/src/controllers/reportController.js
+++ Server/src/controllers/reportController.js
@@ -24,6 +24,8 @@ export const createReport = async (req, res) => {
     res.status(201).json({ msg: "Report was added sucssesfuly", reportId: report._id });
 }
 
+// Read Operations
+
 /**
  * This request handles the default query of a user.
  * When a user logs to the website and navigate to reports, it automatically fetches the most
@@ -41,9 +43,36 @@ export const defaultReport = async (req, res) => {
             msg: "User not found"
         })
     }
-    const storeId = user.mainStore;
+    const store = user.mainStore;
     // search in reports collection by store_id
-    const report = await Report.findOne({store: storeId}).sort({date: -1}).limit(1);
+    const report = await Report.findOne({ store: storeId }).sort({ date: -1 }).limit(1);
     const data = report.hourlyReports.map((rep) => rep);
     res.status(200).json(data);
-}
+};
+
+/**
+ * This method allows us to query the stores reports by date.
+ * In order to do so we need to recieve from the client side the following info:
+ *  1. user_id: to gather the data related to the user
+ *  2. store_id: to get the relevant store
+ *  3. date: to get the relvant date
+ * @param {} req 
+ * @param {*} res 
+ */
+export const qureyReportByDate = async (req, res) => {
+    const userId = req.body.userId;
+    const storeName = req.body.storeName;
+    const date = req.body.date;
+    // check for user
+    const user = await User.findById(userId).populate('stores');
+    if (!user) {
+        return res.status(400).json({
+            success: false,
+            msg: "User not find one."
+        })
+    }
+    // check for store
+    
+    // check for proper date format & extract the date
+    // check for valid date
+};
