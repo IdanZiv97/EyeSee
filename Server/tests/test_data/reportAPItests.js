6bd9ad2e91549bb86bf0d0945048a44873f23798
IdanZiv97 <idanziv7@gmail.com>
2024-11-18 13:55:14 +0200

    tested qurey by dates (server side)

--- Server/tests/test_data/reportAPItests.js
+++ Server/tests/test_data/reportAPItests.js
@@ -15,4 +15,24 @@ const testDefault = async () => {
     console.log(data);
 }
 
-testDefault().catch((error) => console.error('Error:', error));
+/**
+ Tested for valid input and for invalid input
+ */
+const testQueryByDate = async () => {
+    const response = await fetch('http://localhost:4000/report/byDate', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            userId: '6739f3b018369a2a09389057',
+            storeName: 'Berge and Sons',
+            date: '1999-11-18'
+        })
+    })
+    const data = await response.json();
+    console.log(data);
+    
+}
+
+testQueryByDate().catch((error) => console.error('Error:', error));
