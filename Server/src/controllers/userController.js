6668e2438a36399c035687efbd6a5f6be5f95638
IdanZiv97 <idanziv7@gmail.com>
2024-12-03 19:38:03 +0200

    pre-test: logic for updating user info

--- Server/src/controllers/userController.js
+++ Server/src/controllers/userController.js
@@ -33,3 +33,83 @@ export const createUser = async (req, res) => {
         userId: newUser._id,
     })
 }
+
+/**
+ * Function to update the user info
+ * The request body includes:
+ *  1. username
+ *  2. firstName
+ *  3. lastName
+ *  4. email
+ *  5. mainStore: if it fancy to change its main store
+ *  6. userId: to identify
+ * If a field of the user is not being changed it will not be sent in the repsonse and will hold the value of undefined
+ */
+
+export const updateUserInfo = async (req, res) => {
+    // get params
+    try {
+        const userId = req.body.userId;
+        const username = req.body.username;
+        const firstName = req.body.firstName;
+        const lastName = req.body.lastName;
+        const email = req.body.email;
+        const mainStore = req.body.mainStore;
+        // save updates
+        var updates = {};
+        // get user
+        const user = await User.findById(userId).populate('stores').populate('mainStore');
+        // handle possible username changed
+        if (username && !(username === user.username)) {
+            // check if username is already taken
+            const isUsernameTaken = await User.findOne({ username: username });
+            if (isUsernameTaken) {
+                return res.status(400).json({
+                    message: "Username already taken!",
+                    updatedUser: null,
+                })
+            }
+            updates.username = username;
+        }
+        if (email && !(email === user.email)) {
+            // check if email is already taken
+            const isEmailTaken = await User.findOne({ email: email });
+            if (isEmailTaken) {
+                return res.status(400).json({
+                    message: "Email is already taken"
+                })
+            }
+            updates.email = email;
+        }
+        if (mainStore && !(mainStore === user.mainStore.name)) {
+            const newMainStore = user.stores.find((s) => s.name === mainStore);
+            updates.mainStore = newMainStore._id;
+        }
+        if (firstName) {
+            updates.firstName = firstName;
+        }
+        if (lastName) {
+            updates.lastName = lastName;
+        }
+        // update the user
+        const updatedUser = await User.findByIdAndUpdate(userId, updates, { new: true }).populate('mainStore');
+        return res.status(200).json({
+            message: "User updated successfuly",
+            updatedUser: {
+                userId: userId,
+                username: updatedUser.username,
+                firstName: updatedUser.firstName,
+                lastName: updatedUser.lastName,
+                email: updatedUser.email,
+                mainStore: updatedUser.mainStore.name
+            }
+        })
+    } catch (error) {
+        console.error('Error:', error);
+        return res.status(400).json({
+            error: "Iternal Server Error"
+        })
+    }
+
+
+}
--- Server/src/routes/userRouter.js
+++ Server/src/routes/userRouter.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createUser } from "../controllers/userController.js";
+import { createUser, updateUserInfo } from "../controllers/userController.js";
 
 const router = express.Router();
 
 //Route to for signup
 router.post('/signup', createUser);
+router.patch('/user-info', updateUserInfo)
 
 export default router;
