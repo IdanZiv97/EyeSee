fb85527fd5988d1ffd8ab39ad44d0db9b3481f78
IdanZiv97 <idanziv7@gmail.com>
2024-12-03 16:25:29 +0200

    initial logic for deleteing video

--- Server/src/routes/mediaRouter.js
+++ Server/src/routes/mediaRouter.js
@@ -1,10 +1,10 @@
 import express from 'express';
-import { getRecentHeatmap, getHeatmaps, deleteHeatmaps } from '../controllers/mediaController.js';
+import { getRecentHeatmap, getHeatmaps, deleteHeatmaps, deleteVideo } from '../controllers/mediaController.js';
 
 const router = express.Router();
 
 router.post('/heatmap', getRecentHeatmap);
 router.post('/heatmap/byDates', getHeatmaps);
 router.delete('/heatmap/delete', deleteHeatmaps);
-
+router.delete('/video/delete', deleteVideo);
 export default router;
