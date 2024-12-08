edfe38c234f922d09a588006b92e3a25e7c36ad1
IdanZiv97 <idanziv7@gmail.com>
2024-12-08 17:36:12 +0200

    adjusted heatmap model: removed time slice field

--- Server/src/models/heatmapModel.js
+++ Server/src/models/heatmapModel.js
@@ -3,8 +3,7 @@ const Schema = mongoose.Schema;
 const heatmapSchema = new Schema({
     store: { type: mongoose.Schema.Types.ObjectId, ref: "Store", required: true, index: true },
     date: { type: Date, required: true, index: true },
-    timeSlice: { type: String, required: true },
     url: { type: String, required: true }
-}, {timestamps: true});
+}, { timestamps: true });
 
 export const Heatmap = mongoose.model('Heatmap', heatmapSchema);
