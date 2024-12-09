bfd2d59a68d71e6a0cc4f002ab38e1bcddf9e3d0
IdanZiv97 <idanziv7@gmail.com>
2024-12-09 12:44:33 +0200

    basic upload and delete of image, and extraction of public id, works

--- Server/tests/test_data/mediaUploadData/init.js
+++ Server/tests/test_data/mediaUploadData/init.js
@@ -4,15 +4,24 @@ import { v2 as cloudinary } from 'cloudinary';
 import { extractPublicId } from 'cloudinary-build-url' // open-source package to extract publicId from url
 dotenv.config();
 cloudinary.config({
-    cloud_name: "dhvimkgc4",
-    api_key: 875227854865192,
-    api_secret: "rXYdLtTiWyKFtfxt5WdjJQBHSQU"
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDINARY_API_SECRET
 })
 
-const response = await cloudinary.uploader.upload('/Users/idanziv/dev/EyeSee/Server/tests/test_data/mediaUploadData/fake_images/heatmap2.jpeg', {
+const response = await cloudinary.uploader.upload('/Users/idanziv/dev/EyeSee/Server/tests/test_data/mediaUploadData/fake_images/heatmap3.jpeg', {
     asset_folder: 'heatmaps',
-    resource_type: 'image'})
+    resource_type: 'image'
+})
 console.log(response, "\n");
 console.log("URL:", response.url);
 console.log("publicID: ", response.public_id);
-console.log("Extracted publicID: ",extractPublicId(response.url));
+console.log("Extracted publicID: ", extractPublicId(response.url));
+
+const del_resp = await cloudinary.uploader.destroy(
+    extractPublicId(response.url),
+    { resource_type: 'image' }
+)
+
+console.log("Delete response: ", del_resp);
+
