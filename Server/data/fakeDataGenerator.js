749cb4bf3c32967e7504398ee3292b5d54e79bac
IdanZiv97 <idanziv7@gmail.com>
2024-12-16 11:11:39 +0200

    creating data for 3 users

--- Server/data/fakeDataGenerator.js
+++ Server/data/fakeDataGenerator.js
@@ -47,11 +47,11 @@ const endDate = new Date();
 endDate.setMonth(11);
 endDate.setDate(15);
 const usersId = Array.from({ length: 3 }, () => new mongoose.Types.ObjectId());
-const storesIds = Array.from({ length: 10 }, () => new mongoose.Types.ObjectId());
+const storesIds = Array.from({ length: 9 }, () => new mongoose.Types.ObjectId());
 
 // Helper functions
 
-async function createHashedPassowrd(username) {
+async function createHashedPassowrd() {
     const raw = faker.internet.password();
     const salt = await bcrypt.genSalt();
     const hash = await bcrypt.hash(raw, salt);
@@ -91,7 +91,7 @@ user1Stores.forEach(store => fakeStores.push(store));
 var totalReports = Math.floor((endDate - startDate) / (24 * 60 * 60 * 1000));
 
 const user1Reports = user1Stores.flatMap(store => {
-    return Array.from({ length: 12 }, (_, i) => {
+    return Array.from({ length: totalReports }, (_, i) => {
         const hourlyReports = generateHourlyTimeSlices();
         const formattedDate = new Date(startDate.getTime() + i * 24 * 60 * 60 * 1000);
         const dateInFormat = {
@@ -115,7 +115,6 @@ user1Reports.forEach(report => fakeReports.push(report));
 const user1Heatmaps = [];
 // iterate through the stores and for each store upload all the photos
 for (const store of user1Stores) {
-    const currentHeatmaps = [];
     let index = 0;
     for (const path of images_paths) {
         const heatmapId = new mongoose.Types.ObjectId();
@@ -153,7 +152,6 @@ user1Heatmaps.forEach((heatmap) => fakeHeatmaps.push(heatmap));
 // create the jobs
 const user1Jobs = [];
 for (const store of user1Stores) {
-    const currentJobs = [];
     let index = 0;
     for (const path of videos_paths) {
         const jobId = new mongoose.Types.ObjectId();
@@ -192,18 +190,271 @@ for (const store of user1Stores) {
 }
 user1Jobs.forEach(job => fakeJobs.push(job));
 
+// Fake User #2
+
+const fakeUser2 = {
+    _id: { $oid: usersId[1].toString() },
+    username: faker.internet.username(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    password: await createHashedPassowrd(),
+    email: faker.internet.email(),
+    mainStore: { $oid: storesIds[4].toString() },
+    stores: storesIds.slice(3, 6).map(storeId => ({ $oid: storeId.toString() })),
+}
+fakeUsers.push(fakeUser2);
+
+// for each store you should generate the reports seperatyle
+const user2Stores = storesIds.slice(3, 6).map(storeId => ({
+    _id: { $oid: storeId.toString() },
+    name: faker.company.name(),
+    owner: { $oid: fakeUser2._id.$oid },
+    reports: []
+}));
+// add to fake stores
+user2Stores.forEach(store => fakeStores.push(store));
+
+var totalReports = Math.floor((endDate - startDate) / (24 * 60 * 60 * 1000));
+
+const user2Reports = user2Stores.flatMap(store => {
+    return Array.from({ length: totalReports }, (_, i) => {
+        const hourlyReports = generateHourlyTimeSlices();
+        const formattedDate = new Date(startDate.getTime() + i * 24 * 60 * 60 * 1000);
+        const dateInFormat = {
+            $date: { $numberLong: formattedDate.getTime().toString() }
+        };
+        const reportId = new mongoose.Types.ObjectId();
+        const report = {
+            _id: { $oid: reportId.toString() },
+            store: { $oid: store._id.$oid },
+            date: dateInFormat,
+            hourlyReports
+        };
+        store.reports.push({ $oid: reportId.toString() });
+        return report;
+    });
+});
+// add the reports
+user2Reports.forEach(report => fakeReports.push(report));
+
+// create heatmaps
+const user2Heatmaps = [];
+// iterate through the stores and for each store upload all the photos
+const currentHeatmaps = [];
+for (const store of user2Stores) {
+    let index = 0;
+    for (const path of images_paths) {
+        const heatmapId = new mongoose.Types.ObjectId();
+        const formattedDate = new Date(endDate.getTime() - index * 24 * 60 * 60 * 1000);
+        index++;
+        const dateInFormat = {
+            $date: { $numberLong: formattedDate.getTime().toString() }
+        };
+        try {
+            const response = await cloudinary.uploader.upload(
+                path,
+                {
+                    asset_folder: 'heatmaps',
+                    resource_type: 'image'
+                }
+            );
+            // get the url
+            const url = response.url;
+            // create heatmap
+            const new_heatmap = {
+                _id: { $oid: heatmapId.toString() },
+                store: { $oid: store._id.$oid },
+                date: dateInFormat,
+                url: url
+            }
+            user2Heatmaps.push(new_heatmap);
+        } catch (error) {
+            console.error('Error: ', error);
+            process.exit(-1);
+        }
+    }
+}
+user2Heatmaps.forEach((heatmap) => fakeHeatmaps.push(heatmap));
+// create the jobs
+const user2Jobs = [];
+for (const store of user2Stores) {
+    const currentJobs = [];
+    let index = 0;
+    for (const path of videos_paths) {
+        const jobId = new mongoose.Types.ObjectId();
+        const formattedDate = new Date(endDate.getTime() - index * 24 * 60 * 60 * 1000);
+        const dateInFormat = {
+            $date: { $numberLong: formattedDate.getTime().toString() }
+        };
+        try {
+            const response = await cloudinary.uploader.upload(
+                path,
+                {
+                    asset_folder: 'videos',
+                    resource_type: 'video'
+                }
+            );
+            const url = response.url;
+            const uuid = uuidv4();
+            const new_job = {
+                _id: { $oid: jobId.toString() },
+                jobId: uuid,
+                userId: { $oid: store.owner.$oid },
+                storeName: store.name,
+                date: dateInFormat,
+                url: url,
+                startTime: "10:00",
+                endTime: "13:00",
+                length: "180",
+                status: "Processing"
+            }
+            user2Jobs.push(new_job)
+        } catch (error) {
+            console.error('Error: ', error);
+            process.exit(-1);
+        }
+    }
+}
+user2Jobs.forEach(job => fakeJobs.push(job));
+
+// Fake User #3
+
+const fakeUser3 = {
+    _id: { $oid: usersId[2].toString() },
+    username: faker.internet.username(),
+    firstName: faker.person.firstName(),
+    lastName: faker.person.lastName(),
+    password: await createHashedPassowrd(),
+    email: faker.internet.email(),
+    mainStore: { $oid: storesIds[6].toString() },
+    stores: storesIds.slice(6).map(storeId => ({ $oid: storeId.toString() })),
+}
+fakeUsers.push(fakeUser3);
+
+// for each store you should generate the reports seperatyle
+const user3Stores = storesIds.slice(6).map(storeId => ({
+    _id: { $oid: storeId.toString() },
+    name: faker.company.name(),
+    owner: { $oid: fakeUser3._id.$oid },
+    reports: []
+}));
+// add to fake stores
+user2Stores.forEach(store => fakeStores.push(store));
+
+var totalReports = Math.floor((endDate - startDate) / (24 * 60 * 60 * 1000));
+
+const user3Reports = user3Stores.flatMap(store => {
+    return Array.from({ length: totalReports }, (_, i) => {
+        const hourlyReports = generateHourlyTimeSlices();
+        const formattedDate = new Date(startDate.getTime() + i * 24 * 60 * 60 * 1000);
+        const dateInFormat = {
+            $date: { $numberLong: formattedDate.getTime().toString() }
+        };
+        const reportId = new mongoose.Types.ObjectId();
+        const report = {
+            _id: { $oid: reportId.toString() },
+            store: { $oid: store._id.$oid },
+            date: dateInFormat,
+            hourlyReports
+        };
+        store.reports.push({ $oid: reportId.toString() });
+        return report;
+    });
+});
+// add the reports
+user3Reports.forEach(report => fakeReports.push(report));
+
+// create heatmaps
+const user3Heatmaps = [];
+// iterate through the stores and for each store upload all the photos
+for (const store of user3Stores) {
+    let index = 0;
+    for (const path of images_paths) {
+        const heatmapId = new mongoose.Types.ObjectId();
+        const formattedDate = new Date(endDate.getTime() - index * 24 * 60 * 60 * 1000);
+        index++;
+        const dateInFormat = {
+            $date: { $numberLong: formattedDate.getTime().toString() }
+        };
+        try {
+            const response = await cloudinary.uploader.upload(
+                path,
+                {
+                    asset_folder: 'heatmaps',
+                    resource_type: 'image'
+                }
+            );
+            // get the url
+            const url = response.url;
+            // create heatmap
+            const new_heatmap = {
+                _id: { $oid: heatmapId.toString() },
+                store: { $oid: store._id.$oid },
+                date: dateInFormat,
+                url: url
+            }
+            user3Heatmaps.push(new_heatmap);
+        } catch (error) {
+            console.error('Error: ', error);
+            process.exit(-1);
+        }
+    }
+}
+user3Heatmaps.forEach((heatmap) => fakeHeatmaps.push(heatmap));
+// create the jobs
+const user3Jobs = [];
+for (const store of user3Stores) {
+    let index = 0;
+    for (const path of videos_paths) {
+        const jobId = new mongoose.Types.ObjectId();
+        const formattedDate = new Date(endDate.getTime() - index * 24 * 60 * 60 * 1000);
+        const dateInFormat = {
+            $date: { $numberLong: formattedDate.getTime().toString() }
+        };
+        try {
+            const response = await cloudinary.uploader.upload(
+                path,
+                {
+                    asset_folder: 'videos',
+                    resource_type: 'video'
+                }
+            );
+            const url = response.url;
+            const uuid = uuidv4();
+            const new_job = {
+                _id: { $oid: jobId.toString() },
+                jobId: uuid,
+                userId: { $oid: store.owner.$oid },
+                storeName: store.name,
+                date: dateInFormat,
+                url: url,
+                startTime: "10:00",
+                endTime: "13:00",
+                length: "180",
+                status: "Processing"
+            }
+            user3Jobs.push(new_job)
+        } catch (error) {
+            console.error('Error: ', error);
+            process.exit(-1);
+        }
+    }
+}
+user3Jobs.forEach(job => fakeJobs.push(job));
+
 passwordToHashMap.forEach((doc, index) => {
     doc.username = fakeUsers[index].username;
 })
 
 async function saveDataToJSON() {
     try {
-// write each user
+        // Write each user
+        // User1
         await fs.writeFile('./data/users/user1.json', JSON.stringify([fakeUser1], null, 4));
-        console.log('User1 saved to ./data/user1.json');
+        console.log('User1 saved to ./data/users/user1.json');
 
         await fs.writeFile('./data/stores/user1_stores.json', JSON.stringify(user1Stores, null, 4));
-        console.log('User1 stores saved to ./data/stores.json');
+        console.log('User1 stores saved to ./data/stores/user1_stores.json');
         
         await fs.writeFile('./data/reports/user1_reports.json', JSON.stringify(user1Reports, null, 4));
         console.log('User1 reports saved to ./data/reports/user1_reports.json');
@@ -211,9 +462,42 @@ async function saveDataToJSON() {
         await fs.writeFile('./data/heatmaps/user1_heatmaps.json', JSON.stringify(user1Heatmaps, null, 4));
         console.log('User1 heatmaps saved to ./data/heatmaps/user1_heatmaps.json');
 
-        await fs.writeFile('./data/jobs/user1Jobs.json', JSON.stringify(user1Jobs, null, 4));
-        console.log('User1 jobs saved to ./data/jobs.json');
-        // general data
+        await fs.writeFile('./data/jobs/user1_jobs.json', JSON.stringify(user1Jobs, null, 4));
+        console.log('User1 jobs saved to ./data/jobs/user1_jobs.json');
+        
+        // User2 
+        await fs.writeFile('./data/users/user2.json', JSON.stringify([fakeUser2], null, 4));
+        console.log('User1 saved to ./data/users/user2.json');
+
+        await fs.writeFile('./data/stores/user2_stores.json', JSON.stringify(user2Stores, null, 4));
+        console.log('User2 stores saved to ./data/stores/user2_stores.json');
+        
+        await fs.writeFile('./data/reports/user2_reports.json', JSON.stringify(user2Reports, null, 4));
+        console.log('User2 reports saved to ./data/reports/user2_reports.json');
+
+        await fs.writeFile('./data/heatmaps/user2_heatmaps.json', JSON.stringify(user2Heatmaps, null, 4));
+        console.log('User2 heatmaps saved to ./data/heatmaps/user2_heatmaps.json');
+
+        await fs.writeFile('./data/jobs/user2_jobs.json', JSON.stringify(user2Jobs, null, 4));
+        console.log('User2 jobs saved to ./data/jobs/user2_jobs.json');
+        
+        // User 3
+        await fs.writeFile('./data/users/user3.json', JSON.stringify([fakeUser3], null, 4));
+        console.log('User3 saved to ./data/users/user3.json');
+
+        await fs.writeFile('./data/stores/user3_stores.json', JSON.stringify(user3Stores, null, 4));
+        console.log('User3 stores saved to ./data/stores/user3_stores.json');
+        
+        await fs.writeFile('./data/reports/user3_reports.json', JSON.stringify(user3Reports, null, 4));
+        console.log('User3 reports saved to ./data/reports/user3_reports.json');
+
+        await fs.writeFile('./data/heatmaps/user3_heatmaps.json', JSON.stringify(user3Heatmaps, null, 4));
+        console.log('User3 heatmaps saved to ./data/heatmaps/user3_heatmaps.json');
+
+        await fs.writeFile('./data/jobs/user3Jobs.json', JSON.stringify(user3Jobs, null, 4));
+        console.log('User3 jobs saved to ./data/jobs/user3Jobs.json');
+        
+        // General Data
         await fs.writeFile('./data/general/users.json', JSON.stringify(fakeUsers, null, 4));
         console.log('Users saved to ./data/general/users.json');
 
